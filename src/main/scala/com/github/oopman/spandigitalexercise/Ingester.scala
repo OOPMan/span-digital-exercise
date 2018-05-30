a49a6ff975436aa6bbe1a9634c539361c1728557
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2018-05-30 15:23:43 +0200

    Began implementing Ingester. Still needs some exception handling

---
+++ src/main/scala/com/github/oopman/spandigitalexercise/Ingester.scala
@@ -0,0 +1,64 @@
+package com.github.oopman.spandigitalexercise
+
+import java.io.File
+
+import scala.io.Source
+import io.getquill.NamingStrategy
+import com.github.oopman.spandigitalexercise.Constants.Result
+
+import scala.util.matching.Regex
+
+/**
+  * This class encapsulates the logic involved in ingesting input data for Teams
+  * and Results
+  *
+  * @param dao Instance of DAO
+  * @tparam N NamingStrategy type associated with DAO instance
+  */
+class Ingester[N <: NamingStrategy](dao: DAO[N]) {
+  val resultPattern: Regex = raw"(.+) (\d+)".r
+  /**
+    * Ingest data from a Sequence of File instances, returning the count of
+    * Files successfully ingested
+    *
+    * @param files A Sequence of Files
+    * @return Number of files successfully ingested
+    */
+  def ingestFiles(files: Seq[File]): Int = files.map(ingestFile).count(_ == true)
+
+  /**
+    * Ingest data from a single File instance, returning true in the instance of
+    * a successful ingestion
+    *
+    * @param file A File to ingest
+    * @return
+    */
+  def ingestFile(file: File): Boolean = {
+    val lines = Source.fromFile(file).getLines
+    val results = lines
+      // Step 1: Convert lines into Arrays of Options of 2-tuples containing
+      // Team Name and Score
+      .map(_.trim.split(',').map {
+        case resultPattern(teamName, score) => Some((teamName, score.toInt))
+        case _ => None
+      })
+      // Step 2: Convert Arrays of 2-tuples into Lists of Options of 2-tuples
+      // containing Team PK and Result enum
+      .map {
+        case Array(Some((team1Name, team1Score)), Some((team2Name, team2Score))) =>
+          Some(List(
+            (dao.getTeam(team1Name).id, Result.getResult(team1Score, team2Score)),
+            (dao.getTeam(team2Name).id, Result.getResult(team2Score, team1Score))
+          ))
+        case _ => None
+      }
+      // Step 3: Remove any Nones, flatten and insert Result objects
+      .filter(_.isDefined)
+      .flatMap(_.get)
+      .map {
+        case (teamId, result) => dao.addResult(teamId, result)
+      }
+    results.sum > 0
+  }
+
+}

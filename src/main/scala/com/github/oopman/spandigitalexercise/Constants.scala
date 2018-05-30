bccc6aeaadcadec83cc4c6434f8a71a5b41bdd49
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2018-05-30 15:22:51 +0200

    Added getResult utility method to Result enumeration

--- src/main/scala/com/github/oopman/spandigitalexercise/Constants.scala
+++ src/main/scala/com/github/oopman/spandigitalexercise/Constants.scala
@@ -7,5 +7,18 @@ object Constants {
   object Result extends Enumeration {
     type Result = Value
     val Win, Loss, Draw = Value
+
+    /**
+      * Given two score values, determine whether the Result of the 1st vs the 2nd
+      *
+      * @param score1 Primary score value
+      * @param score2 Opposing score value
+      * @return
+      */
+    def getResult(score1: Int, score2: Int): Result = {
+      if (score1 > score2) Win
+      else if (score2 > score1) Loss
+      else Draw
+    }
   }
 }

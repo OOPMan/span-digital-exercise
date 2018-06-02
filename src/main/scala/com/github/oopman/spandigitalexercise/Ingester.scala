9f24afbd966c4e4d2aca8e93914819f21ad6d5e8
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2018-06-02 21:23:53 +0200

    Refactored Ingester

--- src/main/scala/com/github/oopman/spandigitalexercise/Ingester.scala
+++ src/main/scala/com/github/oopman/spandigitalexercise/Ingester.scala
@@ -5,6 +5,7 @@ import java.io.File
 import scala.io.Source
 import io.getquill.NamingStrategy
 import com.github.oopman.spandigitalexercise.Constants.Result
+import com.github.oopman.spandigitalexercise.Constants.Result.Result
 import io.getquill.context.sql.idiom.SqlIdiom
 
 import scala.util.matching.Regex
@@ -14,7 +15,8 @@ import scala.util.matching.Regex
   * and Results
   *
   * @param dao Instance of DAO
-  * @tparam N NamingStrategy type associated with DAO instance
+  * @tparam Dialect SqlIdiom upper-bounded type parameter
+  * @tparam Naming NamingStrategy upper-bounded type parameter
   */
 class Ingester[Dialect <: SqlIdiom, Naming <: NamingStrategy](dao: DAO[Dialect, Naming]) {
   val resultPattern: Regex = raw"(.+) (\d+)".r
@@ -37,29 +39,52 @@ class Ingester[Dialect <: SqlIdiom, Naming <: NamingStrategy](dao: DAO[Dialect, 
   def ingestFile(file: File): Boolean = {
     val lines = Source.fromFile(file).getLines
     val results = lines
-      // Step 1: Convert lines into Arrays of Options of 2-tuples containing
-      // Team Name and Score
-      .map(_.trim.split(',').map {
-        case resultPattern(teamName, score) => Some((teamName, score.toInt))
-        case _ => None
-      })
-      // Step 2: Convert Arrays of 2-tuples into Lists of Options of 3-tuples
-      // containing Team PK and Result enum
-      .map {
-        case Array(Some((team1Name, team1Score)), Some((team2Name, team2Score))) =>
-          Some(List(
-            (dao.getTeam(team1Name).id, Result.getResult(team1Score, team2Score), team1Score),
-            (dao.getTeam(team2Name).id, Result.getResult(team2Score, team1Score), team2Score)
-          ))
-        case _ => None
-      }
-      // Step 3: Remove any Nones, flatten and insert Result objects
+      .map(processLine)
+      .map(processLineAsArray)
       .filter(_.isDefined)
       .flatMap(_.get)
       .map {
-        case (teamId, result, score) => dao.addResult(teamId, result, score)
+        case (teamId, result, score) =>
+          try {
+            dao.addResult(teamId, result, score)
+          } catch {
+            case ex: Exception => 0 // TODO: Handle and log
+          }
       }
     results.sum > 0
   }
 
+  /**
+    * Convert a line into an Array of Optional 2-tuples containing the Team Name
+    * and Score
+    *
+    * @param line A line of text
+    * @return
+    */
+  def processLine(line: String): Array[Option[(String, Int)]] = {
+    line.trim.split(',').map {
+      case resultPattern(teamName, score) => Some((teamName, score.toInt))
+      case _ => None
+    }
+  }
+
+  /**
+    * Convert a value produced by processLine into an Optional List of 3-tuples
+    * containing Team ID, Result value and Team Score. The data in these 3-tuples
+    * is suitable for submission to DAO.addResult
+    *
+    * @param lineAsArray An array of data produced from a line of text by processLine
+    * @return
+    */
+  def processLineAsArray(lineAsArray: Array[Option[(String, Int)]]): Option[List[(Int, Result, Int)]] = {
+    lineAsArray match {
+      case Array(Some((team1Name, team1Score)), Some((team2Name, team2Score))) =>
+        Some(List(
+          (dao.getTeam(team1Name).id, Result.getResult(team1Score, team2Score), team1Score),
+          (dao.getTeam(team2Name).id, Result.getResult(team2Score, team1Score), team2Score)
+        ))
+      case _ =>
+        None
+    }
+  }
 }
